8d58df923f169e417850949cbba5baa4e32ae6cc
Emeric <caramanna.emeric@gmail.com>
2024-12-09 18:12:50 +0100

    Add attack animation (script needed)

--- secret-santa-2024/main_character.gd
+++ secret-santa-2024/main_character.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+const MOVE_TOWARD_SPEED = 25.0
 const JUMP_VELOCITY = -400.0
 @onready var animation: AnimatedSprite2D = $Animation
 const sprite_pixels_offset: int = 7;
@@ -19,20 +20,21 @@ func _physics_process(delta: float) -> void:
 		animation.animation = "falling"
 
 	# Add the gravity.
-	if not is_on_floor():
+	if !is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") && is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("attack") && is_on_floor():
+		animation.animation = "attack"
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, MOVE_TOWARD_SPEED)
 
 	move_and_slide()
 	if velocity.x < 0:
