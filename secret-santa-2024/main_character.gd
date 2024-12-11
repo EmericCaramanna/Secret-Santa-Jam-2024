da66b9c2cf633936cf6f92acf7d297fe94701d03
Emeric <caramanna.emeric@gmail.com>
2024-12-11 11:46:47 +0100

    Add is attacking boolean

--- secret-santa-2024/main_character.gd
+++ secret-santa-2024/main_character.gd
@@ -2,15 +2,15 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const SPEED_DURING_ATTACK = 100.0
+const SPEED_DURING_ATTACK = 30.0
 const MOVE_TOWARD_SPEED = 25.0
 const JUMP_VELOCITY = -400.0
 @onready var animation: AnimatedSprite2D = $Animation
 const sprite_pixels_offset: int = 7;
-
+var is_attacking = false;
 
 func _physics_process(delta: float) -> void:
-	if animation.animation != "attack" && animation.animation != "double_attack":
+	if !is_attacking:
 		if velocity.x != 0 && velocity.y == 0:
 			animation.animation = "running"
 		if velocity.x == 0 && velocity.y == 0:
@@ -32,13 +32,14 @@ func _physics_process(delta: float) -> void:
 			var frame = animation.frame
 			animation.animation = "double_attack"
 			animation.frame = frame
-		if animation.animation != "attack" && animation.animation != "double_attack":
+		if !is_attacking:
 			animation.animation = "attack"
+		is_attacking = true
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		if animation.animation != "attack":
+		if !is_attacking:
 			velocity.x = direction * SPEED
 		else:
 			velocity.x = direction * SPEED_DURING_ATTACK
@@ -55,5 +56,6 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_animation_animation_finished() -> void:
-	if animation.animation == "attack" || animation.animation == "double_attack":
+	if is_attacking:
 		animation.play("default")
+		is_attacking = false;
