d6fa99eac41c66b70de322e3e4e13d4c55dcc687
Emeric <caramanna.emeric@gmail.com>
2024-12-12 15:30:13 +0100

    Add Enemy roaming and attacking

--- secret-santa-2024/enemy.gd
+++ secret-santa-2024/enemy.gd
@@ -1,25 +1,60 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 10.0
 const JUMP_VELOCITY = -400.0
 var hp = 10.0
+var direction = 1
+var is_hurt = false
+var is_attacking = false
 @onready var animation: AnimatedSprite2D = $Animation
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	move_and_slide()
+	if hp > 0 && !is_hurt && !is_attacking:
+		velocity.x = SPEED * direction
+		if !$RayCast2D.is_colliding() && is_on_floor():
+			scale.x = abs(scale.x) * -1
+			direction = -direction
+		move_and_slide()
+		if velocity.x != 0:
+			animation.animation = "running"
+		else:
+			animation.animation = "default"
+		for body in $AttackArea.get_overlapping_bodies():
+			if body.name == "MainCharacter":
+				is_attacking = true
+				animation.play("attacking")
+				break
+
 
 func take_damage(damage: float) -> void:
 	hp -= damage
-	if hp > 0:
-		animation.play("hurting")
-	else:
-		animation.play("dying")
+	is_hurt = true
+	is_attacking = false
+	animation.play("hurting")
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	if animation.animation == "hurting":
+	if is_attacking:
 		animation.play("default")
+		is_attacking = false;
+	if animation.animation == "hurting":
+		is_hurt = false
+		animation.play("default")
+		if hp <= 0:
+			animation.play("dying")
 	elif animation.animation == "dying":
 		queue_free()
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	print(body.name)
+	if body.name == "MainCharacter" && !is_hurt && hp > 0:
+		is_attacking = true
+		animation.play("attacking")
+
+func _on_animation_frame_changed() -> void:
+	if $Animation.animation == "attacking" && $Animation.frame == 7:
+		for body in $AttackArea.get_overlapping_bodies():
+			if body.name == "MainCharacter" && body.has_method("take_damage"):
+				body.take_damage(5)
--- secret-santa-2024/main_character.gd
+++ secret-santa-2024/main_character.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 200.0
 const SPEED_DURING_ATTACK = 30.0
 const MOVE_TOWARD_SPEED = 25.0
 const JUMP_VELOCITY = -400.0
@@ -9,10 +9,11 @@ const JUMP_VELOCITY = -400.0
 const sprite_pixels_offset: int = 7
 const attack_collision_offset: int = 25
 var is_attacking = false
+var is_hurting = false
 @onready var attack_collision: CollisionShape2D = $AttackArea/AttackCollision
 
 func _physics_process(delta: float) -> void:
-	if !is_attacking:
+	if !is_attacking && !is_hurting:
 		if velocity.x != 0 && velocity.y == 0:
 			animation.animation = "running"
 		if velocity.x == 0 && velocity.y == 0:
@@ -35,7 +36,7 @@ func _physics_process(delta: float) -> void:
 		is_attacking = true
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		if !is_attacking:
+		if !is_attacking && !is_hurting:
 			velocity.x = direction * SPEED
 		else:
 			velocity.x = direction * SPEED_DURING_ATTACK
@@ -51,15 +52,21 @@ func _physics_process(delta: float) -> void:
 		animation.position.x = sprite_pixels_offset
 		attack_collision.position.x = attack_collision_offset
 
+func take_damage(damage: float) -> void:
+	animation.play("hurting")
+	is_hurting = true
+	is_attacking = false
+
 func attack() -> void:
 	for area in $AttackArea.get_overlapping_bodies():
-		print("atttack")
-		area.take_damage(5)
+		if area.has_method("take_damage"):
+			area.take_damage(5)
 
 func _on_animation_animation_finished() -> void:
-	if is_attacking:
+	if is_attacking || is_hurting:
 		animation.play("default")
-		is_attacking = false;
+		is_attacking = false
+		is_hurting = false
 
 func _on_animation_frame_changed() -> void:
 	if is_attacking && (animation.frame == 5 || animation.frame == 9):
